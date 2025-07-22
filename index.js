0f42d2cb46f6981c9593a443aec772991c563201
t-ashriya <102204019+t-ashriya@users.noreply.github.com>
2025-07-21 22:03:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,77 +1,64 @@
-// <!-- /**
-//  * WEB222 – Final Assessment
-//  *
-//  * I declare that this assignment is my own work in accordance with
-//  * Seneca Academic Policy. No part of this assignment has been
-//  * copied manually or electronically from any other source
-//  * (including web sites) or distributed to other students.
-//  *
-//  * Please update the following with your information:
-//  *
-//  *      Name:       ASHRIYA TULADHAR
-//  *      Student ID: 168*****
-//  *      Date:       2022/12/07
-//  */
+// WEB222 – Final Assessment
+//
+// I declare that this assignment is my own work in accordance with
+// Seneca Academic Policy. No part of this assignment has been
+// copied manually or electronically from any other source
+// (including web sites) or distributed to other students.
+//
+// Name:       ASHRIYA TULADHAR
+// Student ID: 168*****
+// Date:       2022/12/07
 
+var err = "";
 
-
-var err= "";
-function FormCheck(){
-    err= "";
+function FormCheck() {
+    err = "";
     var ret = false;
-   
     
-  
     PostalCheck();
     
-
-
+    // Check if "Other" is selected and has content
+    var otherRadio = document.getElementById("other");
+    var otherTextarea = document.getElementById("otherReason");
+    if (otherRadio.checked && otherTextarea.value.trim() === "") {
+        err += "Please specify your reason for contacting.<br>";
+    }
 
     document.getElementById("errors").innerHTML = err;
     console.log(err);
 
-    if(err==""){
+    if (err == "") {
         ret = true;
     }
     return ret;
 }
 
-
-
-function PostalCheck()
-{
-    var correct_pattern1= /^[A-Z][0-9][A-Z][0-9][A-Z][0-9]$/;
-    var correct_pattern2= /^[A-Z][0-9][A-Z]\s[0-9][A-Z][0-9]$/;
+function PostalCheck() {
+    var correct_pattern1 = /^[A-Z][0-9][A-Z][0-9][A-Z][0-9]$/;
+    var correct_pattern2 = /^[A-Z][0-9][A-Z]\s[0-9][A-Z][0-9]$/;
 
     var user_postal = document.getElementById("postal").value;
     console.log(user_postal);
 
-    if(correct_pattern1.test(user_postal) == false && correct_pattern2.test(user_postal) == false){
-        err += "Wrong format, Correct Format should be M1M 1M1";
-   }
-
+    if (correct_pattern1.test(user_postal) == false && correct_pattern2.test(user_postal) == false) {
+        err += "Wrong format, Correct Format should be M1M 1M1<br>";
+    }
 }
 
-function radio(val){
-   
-
-        var parent= document.getElementById("textarea1");
+function toggleOtherTextarea() {
+    var otherRadio = document.getElementById("other");
+    var textareaContainer = document.getElementById("otherTextareaContainer");
     
-
-        if (val == "y") 
-        {
-            var ta = document.createElement("textarea");
-            ta.id = "textArea1";
-    
-            parent.appendChild(ta);
-        } 
-          
-        else 
-        {
-           parent.innerHTML="";
-        }
-
-
-
-    
+    if (otherRadio.checked) {
+        textareaContainer.style.display = "block";
+        document.getElementById("otherReason").required = true;
+    } else {
+        textareaContainer.style.display = "none";
+        document.getElementById("otherReason").required = false;
+    }
 }
+
+// Initialize on page load
+document.addEventListener("DOMContentLoaded", function() {
+    toggleOtherTextarea(); // Set initial state
+});
